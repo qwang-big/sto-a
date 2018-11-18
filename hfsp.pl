80255f558383a2836b7c5d7b05b46cf5c1bbdad8
Herrmann <herrmann@dhcp653-033.laptop-wlc.uni-heidelberg.de>
2018-11-18 16:02:33 +0100

    correct dividende

--- hfsp.pl
+++ hfsp.pl
@@ -3,6 +3,6 @@ while(<>){
 print "{$1" if /("Code":\S+?,)/;
 print "$1\"," if /("CQCXR":"\S{10})/;
 print $1 if /("EarningsPerShare":\S+?,)/ or /("JLYTBZZ":\S+?,)/;
-print "$1}," if /("AllocationPlan":\S+?),/;
+print "$1}," if /("AllocationPlan":\S+?"),/;
 }
 print ']';
