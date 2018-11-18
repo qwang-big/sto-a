c6e48d24652cb8674440f608bac3f265e6941a00
Herrmann <herrmann@dhcp653-033.laptop-wlc.uni-heidelberg.de>
2018-11-18 15:51:46 +0100

    simplify dividende

---
+++ hfsp.pl
@@ -0,0 +1,8 @@
+print '[';
+while(<>){
+print "{$1" if /("Code":\S+?,)/;
+print "$1\"," if /("CQCXR":"\S{10})/;
+print $1 if /("EarningsPerShare":\S+?,)/ or /("JLYTBZZ":\S+?,)/;
+print "$1}," if /("AllocationPlan":\S+?),/;
+}
+print ']';
